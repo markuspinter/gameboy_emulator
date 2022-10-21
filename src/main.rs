b98222b34a8021f95f918e8323bc6c3118e53ea1
markuspinter <markus.m.pinter@gmail.com>
2022-10-21 20:21:25 +0200

    add args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::env;
+
 pub mod gameboy;
 
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 
     let gb = gameboy::Gameboy::new("".into());
 
